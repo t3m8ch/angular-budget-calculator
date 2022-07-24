f515e2641b1bec1a841909d03573e0ef22657cf1
Kudyakov Artem <t3m8ch@yandex.ru>
2022-07-24 22:32:57 +0400

    build: Add taiga-ui

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,5 @@
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { TuiRootModule } from "@taiga-ui/core";
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
@@ -5,7 +7,7 @@ import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, TuiRootModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
