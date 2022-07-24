64f10b8a77bb67943906e881955acdabf9930311
Kudyakov Artem <t3m8ch@yandex.ru>
2022-07-24 22:59:42 +0400

    feat: Layout page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,15 @@
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { TuiRootModule } from "@taiga-ui/core";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TuiButtonModule, TuiRootModule } from '@taiga-ui/core';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TuiIslandModule } from '@taiga-ui/kit';
+import { TuiMoneyModule } from '@taiga-ui/addon-commerce';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, TuiRootModule, BrowserAnimationsModule],
+  imports: [BrowserModule, TuiRootModule, BrowserAnimationsModule, TuiIslandModule, TuiMoneyModule, TuiButtonModule],
   providers: [],
   bootstrap: [AppComponent],
 })
