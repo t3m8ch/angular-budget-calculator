4d8840bdb75ad0e95b76e8f157db321745f86ca5
Kudyakov Artem <t3m8ch@yandex.ru>
2022-07-25 14:46:52 +0400

    feat: Add id to models

--- src/app/revenues-list/revenue.model.ts
+++ src/app/revenues-list/revenue.model.ts
@@ -1,6 +1,9 @@
 import bignumber from 'bignumber.js';
+import { v4 as uuidv4 } from 'uuid';
 
 export class RevenueModel {
+  public id: string = uuidv4();
+
   constructor(public title: string, public amount: bignumber) {
   }
 }
